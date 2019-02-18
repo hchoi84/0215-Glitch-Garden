b61c7ccb174a2824137833eeca2f04a0a2983c09
Howard Choi <hchoi84@gmail.com>
2019-02-18 13:18:25 -0800

    Implement Star Display

---
+++ Assets/StarDisplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int stars = 100;
+    private TextMeshProUGUI starText = default;
+
+    private void Start()
+    {
+        starText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+    }
+}
